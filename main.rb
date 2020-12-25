2e13fc9bc9c4fa49079538fa9445c65536af4868
Levvine <ahuan38@emory.edu>
2020-12-25 13:08:45 -0500

    completed
    patch 10.25.1

--- main.rb
+++ main.rb
@@ -1,23 +1,41 @@
 require_relative 'riot-api'
 
 api = RiotAPI.new
-summoner_name = 'levin'
+summoner_name = 'pants are dragon'
 escaped_name = CGI.escape(summoner_name)
 puts "#{escaped_name}"
 
 # ex1, fetching summoner data
-accountId = api.getAccountID(escaped_name)
+accountId = api.getAccountId(escaped_name)
+summonerId = api.getSummonerId(escaped_name)
 puts "accountId: #{accountId}"
+puts "summonerId: #{summonerId}"
 
-# ex2, fetch match history
-matchId = api.getLastMatchId(accountId)
-puts "matchId: #{matchId}"
+# remove this while loop
+while false
+  # ex2, fetch match history
+  matchId = api.getLastMatchId(accountId)
+  puts "matchId: #{matchId}"
 
-# ex3, fetch match data
-participants = api.getTeamParticipants(matchId)
-team1 = participants.slice(0...4).join(", ")
-team2 = participants.slice(5...9).join(", ")
-puts "team1: #{team1}"
-puts "team2: #{team2}"
+  # ex3, fetch match data
+  participants = api.getTeamParticipants(matchId)
+  team1 = participants.slice(0...4).join(", ")
+  team2 = participants.slice(5...9).join(", ")
+  puts "team1: #{team1}"
+  puts "team2: #{team2}"
 
-# ex4, look up a match-in-progress
+  # ex4, look up a match-in-progress
+  matchInProgress = api.getMatchInProgress(escaped_name)
+  puts "gameId: #{matchInProgress}"
+end
+
+# ex5a, get champion mastery by player id and champion id
+# champ_name = "Ashe"
+# champId = api.getChampId(champ_name)
+# masteryByChamp = api.getMasteryByChamp(summonerId, champId)
+# puts "#{champ_name} Mastery Level: #{masteryByChamp}"
+
+# ex5, get top three masteries by points
+puts "Top 3 Masteries:"
+leaderboard = api.getTopMasteries(summonerId)
+leaderboard.each { |m| puts "#{m[0]}: #{m[1]}" }
--- riot-api.rb
+++ riot-api.rb
@@ -15,7 +15,7 @@ class RiotAPI
   attr_accessor :routing_value
 
   # gets account id from summoner name
-  def getAccountID(name)
+  def getAccountId(name)
     request = "/lol/summoner/v4/summoners/by-name/#{name}"
     uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
     json = Net::HTTP.get(uri)
@@ -38,10 +38,97 @@ class RiotAPI
     uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
     json = Net::HTTP.get(uri)
     response = JSON.parse(json)
-    
+
     participants = response["participantIdentities"].map do |p|
       p["player"]["summonerName"]
     end
+  end
+
+  def getSummonerId(name)
+    request = "/lol/summoner/v4/summoners/by-name/#{name}"
+    uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
+    json = Net::HTTP.get(uri)
+    response = JSON.parse(json)
+
+    summonerId = response["id"]
+  end
+
+  def getMatchInProgress(name)
+    summonerId = self.getSummonerId(name)
+    request = "/lol/spectator/v4/active-games/by-summoner/#{summonerId}"
+    uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
+    httpResponse = Net::HTTP.get_response(uri)
+    case httpResponse
+    when Net::HTTPNotFound
+      return "no game in progress"
+    when Net::HTTPSuccess
+      json = httpResponse.body
+      response = JSON.parse(json)
+      gameIdInProgress = response["gameId"]
+      gameModeInProgress = response["gameMode"]
+      "#{gameIdInProgress} - #{gameModeInProgress}"
+    end
+  end
+
+  def getLatestVersion
+    request = "https://ddragon.leagueoflegends.com/api/versions.json"
+    uri = URI(request)
+    version = JSON.parse(Net::HTTP.get(uri))[0]
+  end
+
+  def parseChampIdJson(fileName)
+    if !File.exist?(fileName)
+      champIdsFile = File.new(fileName,"w")
+      version = self.getLatestVersion
+      request = "https://ddragon.leagueoflegends.com/cdn/#{version}/data/en_US/champion.json"
+      uri = URI(request)
+      json = Net::HTTP.get(uri)
+      response = JSON.parse(json)["data"]
+
+      champIdHash = Hash.new
+      response.each do |c|
+        champIdHash[c[0]] = c[1]["key"]
+      end
+      champIdsFile.write(champIdHash.to_json)
+      champIdHash
+    else
+      json = File.read(fileName)
+      champIdHash = JSON.parse(json)
+    end
+  end
+
+  def getChampId(name)
+    champIdHash = parseChampIdJson("champIds.json")
+    champIdHash[name]
+  end
+
+  def getChampName(id)
+    champIdHash = parseChampIdJson("champIds.json")
+    champIdHash.key(id)
+  end
+
+  def getMasteryByChamp(summonerId, champId)
+    request = "/lol/champion-mastery/v4/champion-masteries/by-summoner/#{summonerId}/by-champion/#{champId}"
+    uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
+    json = Net::HTTP.get(uri)
+    response = JSON.parse(json)
+
+    response["championLevel"]
+  end
+
+  def getTopMasteries(summonerId)
+    request = "/lol/champion-mastery/v4/champion-masteries/by-summoner/#{summonerId}"
+    uri = URI("#{self.routing_value+request}?api_key=#{$API_KEY}")
+    json = Net::HTTP.get(uri)
+    response = JSON.parse(json).slice(0..2)
+
+    leaderboard = Hash.new
+    response.each do |m|
+      champName = self.getChampName(m["championId"].to_s)
+      mastery = m["championLevel"]
+      leaderboard[champName] = mastery
+    end
+    leaderboard
   end
 
 end
